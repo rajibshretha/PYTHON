95edcfb76e2dd1657a8f89d985421626634a1edd
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-07-30 15:16:51 +0545

    after schoool

--- day_2.py
+++ day_2.py
@@ -1,3 +1,14 @@
+# name="Ra   asdf   jib"
+# print(name[1:3])
+# print(name[1:])
+# print(name[1:6:2])
+# x=name.endswith("b")
+# print(x)
+# y=name.endswith("r")
+# print(y)
+# print(name.find("asdf"))
+
+
 # name="rajib shresthaa  shrestha"
 # print(name.find("  "))
 # print(name.replace("rajib","rajesh")) 
@@ -11,7 +22,26 @@
 # fruits.sort()
 # print(fruits)
 
-l=[2,35,1,2,1]
-print(sum(l))
+# l=[2,35,1,2,1]
+# print(sum(l))
 
-print(l.count(1))
+# print(l.count(1))
+
+list=["Rajib","shrestha","Stha"]
+list.append("RAJIB")
+print(list)
+list.sort()
+print(list)
+list.reverse()
+print(list)
+list.insert(3,"HELLO")
+print(list)
+list.pop(3)
+print(list)
+
+tuple=("rajib","shrestha","rajib");
+print(tuple.count("rajib"))
+print(tuple)
+print(len(tuple))
+a,b,c=tuple
+print(a,b,c)
