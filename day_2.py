bbf2d300c48ba8a123296d779a8b13b12023470e
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-07-30 10:05:00 +0545

    day_2

---
+++ day_2.py
@@ -0,0 +1,17 @@
+# name="rajib shresthaa  shrestha"
+# print(name.find("  "))
+# print(name.replace("rajib","rajesh")) 
+
+
+# fruits=[]
+# fruits.append(int(input("Enter marks of  1 student")))
+# fruits.append(int(input("Enter marks of  2 student")))
+# fruits.append(int(input("Enter marks of  3 student")))
+# fruits.append(int(input("Enter marks of  4 student")))
+# fruits.sort()
+# print(fruits)
+
+l=[2,35,1,2,1]
+print(sum(l))
+
+print(l.count(1))
