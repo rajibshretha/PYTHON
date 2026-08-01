d5cfd24c6fe57d5570f107228ef9dc366e2f5ad5
Rajib Shrestha <rajibshrestha010@gmail.com>
2026-08-01 22:02:50 +0545

    day 4

---
+++ day_4.py
@@ -0,0 +1,93 @@
+# codition
+
+# a=int(input("Enter a number "))
+# if(a%2==0):
+#     print("The given number is even ")
+# else:
+#     print("The given number is odd ")
+
+# if(4>3 and 34>34 or 34>23 ):
+#     print("true")
+  
+  
+# p1
+
+# un=input("Enter a usename : ")
+# n=len(un)
+# if(n>10):
+#     print("the username contain more then 10 letter")
+# else:
+#     print("Enter username again")
+
+# p2
+
+# text=input("enter a post ")
+# if("Rajib".lower() in text.lower()):
+#     print("yeah it contain rajib")
+# else:
+#     print("nope it doesnt contain rajib")
+
+# loops
+
+# for i in range(1,5):
+#     print(i)
+    
+    
+# list=[1,"rajib","shrestha",34.34]
+# i=0
+# while(i<len(list)):
+#     print(list[i])
+#     i+=1
+    
+# for i in (list):
+#     print(i)
+
+# p1
+# n=int(input("enter a number"))
+# for i in range (1,11):
+#     print(f"{n}*{i}={n*i}")
+
+# p2
+# l=["Harry","soham","sachim","rajib"]
+
+# for name in l:
+#     if(name.startswith("s" or "s")):
+#         print(name)
+    
+# p3
+
+# n=int(input("enter a number"))
+# c=0
+# while(n!=0):
+#     c=c+n
+#     n-=1
+
+# print(c)
+
+
+# p5 making a triangle
+
+# n=int(input("enter a nubmer"))
+# for i in range ( 1, n+1):
+#     print(" "*(n-i),end="")
+#     print("*"*(2*i-1),end="")
+    
+
+# p6 hallow square
+
+# n=int(input("enter a nubmer"))
+# for i in range ( 1, n+1):
+#    if(i==1 or i==n):
+#        print("*"* n)
+#    else:
+#        print("*",end="")
+#        print(" "*(n-2),end="")
+#        print("*",end="")
+#        print("")
+ 
+    
+# p4
+n=int(input("enter a number"))
+for i in range (1,11):
+    print(f"{n}*{11-i}={n*(11-i)}")
+    
